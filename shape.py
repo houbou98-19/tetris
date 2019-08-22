dc13a7fd4ebc08bcca3e1d76b2b113751d260289
Housam Boughdadi <houbou98@gmail.com>
2019-08-22 10:21:39 +0200

    Signed-off-by: Housam Boughdadi <houbou98@gmail.com>

--- shape.py
+++ shape.py
@@ -1,2 +1,15 @@
-class Myclass():
-	pass
+class Shape:
+	def __init__(self, position, type, color):
+		self.position = position
+		self.type = type
+		self.color = color
+		
+	def move():
+		pass
+		
+	def rotate():
+		pass
+	
+	def collision():
+		pass
+		
