fee5761b28daa1eb0934e25d974e72cd6f8cde91
Housam Boughdadi <houbou98@gmail.com>
2019-08-22 21:51:51 +0200

    Signed-off-by: Housam Boughdadi <houbou98@gmail.com>

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import time
+
+from shape import *
+
+x = O([3,4], 1, "O", [255,0,0])
+
+x.who()
+
+while True:
+	time.sleep(10)
+	break
+
+'''
+pygame.init()
+display = pygame.display.set_mode((800,800))
+
+while True:
+	time.sleep(5)
+	break
+	
+'''
--- main.pyw
+++
@@ -1,7 +0,0 @@
-import pygame
-
-pygame.init()
-display = pygame.display.set_mode((800,800))
-
-while True:
-	pass
